6d2ea01faaf08d37da5758cdf87f10e37f7e96e9
Steven Kalt <kalt.steven@gmail.com>
2025-09-06 09:41:48 -0400

    fix: uv sync syntax

--- wrappers/python/scripts/ci/github/activate_venv.sh
+++ wrappers/python/scripts/ci/github/activate_venv.sh
@@ -13,13 +13,8 @@ fi
 
 # Ensure binaries from the virtualenv are available at the start of $PATH
 # see https://docs.python.org/3/library/venv.html#creating-virtual-environments
-if [ -d "$VIRTUAL_ENV/bin" ]; then
-  # on unix systems, virtualenv puts executables in .venv/bin
-  venv_bin_path="$VIRTUAL_ENV/bin"
-elif [ -d "$VIRTUAL_ENV/Scripts" ]; then
+# on unix systems, virtualenv puts executables in .venv/bin
+echo "$VIRTUAL_ENV/bin" >> "$GITHUB_PATH"
   # on windows, virtualenv places executables in .venv/Scripts
-  venv_bin_path="$VIRTUAL_ENV/Scripts"
-fi
-
-echo "$venv_bin_path" >> "$GITHUB_PATH"
+echo "$VIRTUAL_ENV/Scripts" >> "$GITHUB_PATH"
 # see https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#adding-a-system-path
